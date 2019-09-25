880ca561483bcbb6e776ad45922d489c07fd9e32
chuangmaster <masterchuang@urtody.com>
2019-09-25 13:14:15 +0800

    init site map creator

--- SitemapCreator/Program.cs
+++ SitemapCreator/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace SitemapCreator
 {
@@ -10,6 +14,37 @@ namespace SitemapCreator
     {
         static void Main(string[] args)
         {
+            //取得網站資料
+            var webSiteData = GetWebSiteData();
+
+            //產生 site map xml
+            XMLCreator ctr = new XMLCreator();
+            var xmlData = ctr.GetSiteMapXML(webSiteData);
+            //產生 xml
+            CreateXMLFile(xmlData);
+
+            Console.WriteLine($"Site map產生完畢");
+        }
+
+        /// <summary>
+        /// Get web site data
+        /// </summary>
+        /// <returns></returns>
+        private static Dictionary<string, float> GetWebSiteData()
+        {
+            var result = new Dictionary<string, float>();
+            result.Add("https://homepage.com.tw", 1.0f);
+            result.Add("https://homepage.com.tw/news", 0.8f);
+            return result;
+        }
+
+        private static void CreateXMLFile(XElement data)
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "sitemap.xml");
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                sw.WriteLine(data.ToString());
+            }
         }
     }
 }
---
+++ SitemapCreator/XMLCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+namespace SitemapCreator
+{
+    internal class XMLCreator
+    {
+        private XElement _xml;
+
+        public XMLCreator()
+        {
+            _xml = new XElement("urlset");
+        }
+        internal XElement GetSiteMapXML(Dictionary<string, float> webSiteData)
+        {
+            foreach (var item in webSiteData)
+            {
+                _xml.Add(new XElement("url", 
+                    new XElement("loc", item.Key), 
+                    new XElement("lastmod", DateTime.UtcNow), 
+                    new XElement("priority", item.Value)));
+            }
+            return _xml;
+        }
+    }
+}
